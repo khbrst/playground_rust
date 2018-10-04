a540c06328ebd0806c797fa116ea3b2a7d25e702
Yoon Youngho <khbrst@gmail.com>
2018-10-04 21:58:33 +0900

    Update patterns module

--- basics/src/lib.rs
+++ basics/src/lib.rs
@@ -4,27 +4,41 @@ mod patterns;
 mod smart_pointers;
 
 pub fn test_smart_pointers() {
+  println!("* test_smart_pointers");
   smart_pointers::box_save_to_heap();
   smart_pointers::implement_cons_list_use_box();
   smart_pointers::implement_custom_smart_pointer();
+  println!("");
 }
 
 pub fn test_concurrency() {
+  println!("* test_concurrency");
   concurrency::threads::value_move_into_closure();
   concurrency::message_passing::send_single_message();
   concurrency::message_passing::send_multiple_messages();
   concurrency::message_passing::send_messages_from_multiple_producer();
   concurrency::share::lock_use_mutex();
+  println!("");
 }
 
 pub fn test_oop() {
+  println!("* test_oop");
   oop::implementing_the_trait();
   oop::implementing_state_design_pattern_as_trait();
   oop::implementing_state_design_pattern_as_type();
+  println!("");
 }
 
 pub fn test_patterns() {
+  println!("* test_patterns");
   patterns::destructure_structs();
   patterns::destructure_structs_in_match();
   patterns::destructure_enums();
+  patterns::destructure_reference();
+  patterns::destructure_structs_and_tuples();
+  patterns::create_reference();
+  patterns::create_mutable_reference();
+  patterns::match_guards();
+  patterns::at_operator_bindings();
+  println!("");
 }
--- basics/src/patterns.rs
+++ basics/src/patterns.rs
@@ -43,3 +43,86 @@ pub fn destructure_enums() {
     }
   }
 }
+
+#[allow(unused_variables)]
+pub fn destructure_reference() {
+  let points = vec![
+    Point { x: 0, y: 0 },
+    Point { x: 1, y: 5 },
+    Point { x: 10, y: -3 },
+  ];
+
+  let sum_of_squares: i32 = points.iter().map(|&Point { x, y }| x * x + y * y).sum();
+}
+
+#[allow(unused_variables)]
+pub fn destructure_structs_and_tuples() {
+  let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
+}
+
+pub fn create_reference() {
+  let robot_name = Some(String::from("Bors"));
+
+  match robot_name {
+    Some(ref name) => println!("Found a name: {}", name),
+    None => (),
+  }
+
+  println!("robot_name is: {:?}", robot_name);
+}
+
+pub fn create_mutable_reference() {
+  let mut robot_name = Some(String::from("Bors"));
+
+  match robot_name {
+    Some(ref mut name) => *name = String::from("Another name"),
+    None => (),
+  }
+
+  println!("robot_name is: {:?}", robot_name);
+}
+
+pub fn match_guards() {
+  let num = Some(4);
+
+  match num {
+    Some(x) if x < 5 => println!("less than five: {}", x),
+    Some(x) => println!("{}", x),
+    None => (),
+  }
+
+  let x = Some(5);
+  let y = 10;
+
+  match x {
+    Some(50) => println!("Got 50"),
+    Some(n) if n == y => println!("Matched, n = {:?}", n),
+    _ => println!("Default case, x = {:?}", x),
+  }
+
+  println!("at the end: x = {:?}, y = {:?}", x, y);
+
+  let x = 4;
+  let y = false;
+
+  match x {
+    4 | 5 | 6 if y => println!("yes"),
+    _ => println!("no"),
+  }
+}
+
+pub fn at_operator_bindings() {
+  enum Message {
+    Hello { id: i32 },
+  }
+
+  let msg = Message::Hello { id: 5 };
+
+  match msg {
+    Message::Hello {
+      id: id_variable @ 3...7,
+    } => println!("Found an id in range: {}", id_variable),
+    Message::Hello { id: 10...12 } => println!("Found an id in another range"),
+    Message::Hello { id } => println!("Found some other id: {}", id),
+  }
+}
