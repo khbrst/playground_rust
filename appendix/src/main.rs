6797a0784bf48bc8601294e6e429f6bf25507e8e
Yoon Youngho <khbrst@gmail.com>
2018-10-07 16:35:05 +0900

    Create macro projects

--- appendix/src/main.rs
+++ appendix/src/main.rs
@@ -1,3 +1,20 @@
+#[macro_use]
+extern crate appendix;
+extern crate hello_macro;
+
+use hello_macro::HelloMacro;
+
+struct Pancakes;
+
+impl HelloMacro for Pancakes {
+  fn hello_macro() {
+    println!("Hello, Macro! My name is Pancakes!");
+  }
+}
+
 fn main() {
-    println!("Hello, world!");
+  let declarative_macro = my_vec!(1, 2, 3);
+  println!("{:?}", declarative_macro);
+
+  Pancakes::hello_macro();
 }
